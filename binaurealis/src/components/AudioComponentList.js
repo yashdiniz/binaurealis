19b07a6ecf44b904148f3db457f4aad2ef85a761
Yash Diniz <yashdiniz@gmail.com>
2020-07-29 17:41:21 +0530

    addAudioComponent to list Made provision to add new audio components to the list of components.

--- binaurealis/src/components/AudioComponentList.js
+++ binaurealis/src/components/AudioComponentList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AudioComponent from './AudioComponent';
 
 class AudioComponentList extends Component {
     constructor(props) {
@@ -8,20 +9,27 @@ class AudioComponentList extends Component {
             children: this.props.children
         };
     }
-    
-    addAudioComponent(e){
 
+    addAudioComponent(){
+        const list = [].concat(this.state.children);
+        list.push(
+            <AudioComponent 
+                key={list.length} gain={0.1} frequency={880} offset={-5}
+                stopPlay={this.props.stopPlay.bind(this)}
+            />
+        );
+        this.setState({children: list}); 
     }
 
     // TODO: work on add new AudioComponent button stub!
     render() {
         return (
             <>
-            {this.state.children}
-            <button
-                className="frui-btn frui-btn--fab"
-                onClick={this.addAudioComponent.bind(this)}
-            >+</button>
+                {this.state.children}
+                <button
+                    className="frui-btn frui-btn--fab"
+                    onClick={this.addAudioComponent.bind(this)}
+                >Add</button>
             </>
         );
     }
