c0c9d4d39c54ccf3f2c3712f7c1e2b7193fdf88d
Yash Diniz <yashdiniz@gmail.com>
2020-11-23 20:10:18 +0530

    Fixed URLs for GH pages.

--- docs/precache-manifest.bdedf3ce397d5550b07bdce10c9cf7a8.js
+++ docs/precache-manifest.bdedf3ce397d5550b07bdce10c9cf7a8.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "1bc512fcea7eecae3123f989d663efab",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "db0f7017b8e8ab4f242c",
-    "url": "/static/js/2.4ac91a40.chunk.js"
+    "url": "static/js/2.4ac91a40.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.4ac91a40.chunk.js.LICENSE.txt"
+    "url": "static/js/2.4ac91a40.chunk.js.LICENSE.txt"
   },
   {
     "revision": "bd8393ffae60a537d42b",
-    "url": "/static/js/main.17af7c66.chunk.js"
+    "url": "static/js/main.17af7c66.chunk.js"
   },
   {
     "revision": "4792740d13dcad8ef4fe",
-    "url": "/static/js/runtime-main.9a943c52.js"
+    "url": "static/js/runtime-main.9a943c52.js"
   }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdedf3ce397d5550b07bdce10c9cf7a8.js"
+  "precache-manifest.bdedf3ce397d5550b07bdce10c9cf7a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
