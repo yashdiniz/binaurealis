53935a7855345a3a22b1b1fb35290779aada3cd4
Yash Diniz <yashdiniz@gmail.com>
2020-11-23 19:59:56 +0530

    build folder now under /docs of repo

---
+++ docs/precache-manifest.bdedf3ce397d5550b07bdce10c9cf7a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc512fcea7eecae3123f989d663efab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0f7017b8e8ab4f242c",
+    "url": "/static/js/2.4ac91a40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ac91a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8393ffae60a537d42b",
+    "url": "/static/js/main.17af7c66.chunk.js"
+  },
+  {
+    "revision": "4792740d13dcad8ef4fe",
+    "url": "/static/js/runtime-main.9a943c52.js"
+  }
+]);
